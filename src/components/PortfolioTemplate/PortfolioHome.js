cfb9939b5d9d6825d4b37da1380f0e722ca378e7
sajid1545 <clashking1545@gmail.com>
2023-05-19 17:50:46 +0600

    template updated

--- src/components/PortfolioTemplate/PortfolioHome.js
+++ src/components/PortfolioTemplate/PortfolioHome.js
@@ -1,42 +1,67 @@
 import React, { useContext } from 'react';
 import { COLOR_CONTEXT } from './../../context/ColorProvider';
+import PortfolioAbout from './PortfolioAbout';
 import PortfolioNavbar from './PortfolioNavbar';
+import PortfolioProjects from './PortfolioProjects';
 import PortfolioSkills from './PortfolioSkills';
 
 export default function PortfolioHome() {
 	const { backgroundColor, primaryColor, secondaryColor, fontColor, fileList } =
 		useContext(COLOR_CONTEXT);
 
+	const [showAbout, setShowAbout] = React.useState(false);
+	const [showProjects, setShowProjects] = React.useState(false);
+	const [showSkills, setShowSkills] = React.useState(false);
+
 	return (
-		<div className="" style={{ backgroundColor: backgroundColor,color:primaryColor }}>
-			<PortfolioNavbar />
-			<div className="flex items-center justify-between gap-5 w-5/6 mx-auto mt-20">
-				<div className="p-5 lg:w-2/4">
-					{/* Here name , designation will be dynamic */}
-					<h4>Hello</h4>
-					<h2 className="text-3xl">I'm Mark Andrew</h2>
-					<h3 className="text-xl">A passionate {}</h3>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-						incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-						exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-						dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-						mollit anim id est laborum
-					</p>
-				</div>
-				<div className="lg:w-2/4 flex justify-center">
-					{/* The image will be dynamic */}
-					{fileList[0]?.thumbUrl ? (
-						<img src={fileList[0]?.thumbUrl} alt="" className="w-[300px] h-[300px]" />
-					) : (
-						<img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnN8ZW58MHx8MHx8&w=1000&q=80" className='w-[300px] h-[300px]' alt='person' />
-					)}
-				</div>
-			</div>
-			<div>
+		<div className="" style={{ backgroundColor: backgroundColor, color: primaryColor }}>
+			<PortfolioNavbar
+				setShowAbout={setShowAbout}
+				setShowProjects={setShowProjects}
+				setShowSkills={setShowSkills}
+			/>
+
+			{showAbout ? (
+				<PortfolioAbout />
+			) : showSkills ? (
 				<PortfolioSkills />
-			</div>
+			) : showProjects ? (
+				<PortfolioProjects />
+			) : (
+				<>
+					<div className="flex items-center justify-between gap-5 w-5/6 mx-auto mt-20">
+						<div className="p-5 lg:w-2/4">
+							{/* Here name , designation will be dynamic */}
+							<h4>Hello</h4>
+							<h2 className="text-3xl">I'm Mark Andrew</h2>
+							<h3 className="text-xl">A passionate {}</h3>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+								incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+								exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+								irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+								pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+								deserunt mollit anim id est laborum
+							</p>
+						</div>
+						<div className="lg:w-2/4 flex justify-center">
+							{/* The image will be dynamic */}
+							{fileList[0]?.thumbUrl ? (
+								<img src={fileList[0]?.thumbUrl} alt="" className="w-[300px] h-[300px]" />
+							) : (
+								<img
+									src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnN8ZW58MHx8MHx8&w=1000&q=80"
+									className="w-[300px] h-[300px]"
+									alt="person"
+								/>
+							)}
+						</div>
+					</div>
+					<div>
+						<PortfolioSkills />
+					</div>
+				</>
+			)}
 		</div>
 	);
 }
--- src/components/PortfolioTemplate/PortfolioNavbar.js
+++ src/components/PortfolioTemplate/PortfolioNavbar.js
@@ -1,36 +1,54 @@
 import React, { useContext, useState } from 'react';
 import { COLOR_CONTEXT } from './../../context/ColorProvider';
 
-export default function PortfolioNavbar() {
+export default function PortfolioNavbar({ setShowAbout, setShowProjects, setShowSkills }) {
+	const { backgroundColor, primaryColor, secondaryColor, fileList, fontColor } =
+		useContext(COLOR_CONTEXT);
 
-  const {
-		backgroundColor,
-		primaryColor,
-    secondaryColor,
-    fileList,
-		fontColor,
-	} = useContext(COLOR_CONTEXT);
-
-  return (
-    <header style={{ color: fontColor, backgroundColor: primaryColor }} className="bg-blue-400">
-      <div className="flex w-5/6 justify-between mx-auto py-5 items-center">
-      <div>
-						{fileList.length ? (
-							<img src={fileList[0]?.thumbUrl} alt="" className="w-16 h-16 rounded-full" />
-						) : (
-							<h1 className="font-bold text-[25px]">LOGO</h1>
-						)}
-					</div>
-        <div>
-          <ul className="flex gap-5">
-            <li className="font-semibold cursor-pointer hover:text-white">Home</li>
-            <li className="font-semibold cursor-pointer hover:text-white">About</li>
-            <li className="font-semibold cursor-pointer hover:text-white">Skill</li>
-            <li className="font-semibold cursor-pointer hover:text-white">Education</li>
-            <li className="font-semibold cursor-pointer hover:text-white">Contact</li>
-          </ul>
-        </div>
-      </div>
-    </header>
-  );
+	return (
+		<header style={{ color: fontColor, backgroundColor: primaryColor }} className="bg-blue-400">
+			<div className="flex w-5/6 justify-between mx-auto py-5 items-center">
+				<div>
+					{fileList.length ? (
+						<img src={fileList[0]?.thumbUrl} alt="" className="w-16 h-16 rounded-full" />
+					) : (
+						<h1 className="font-bold text-[25px]">LOGO</h1>
+					)}
+				</div>
+				<div>
+					<ul className="flex gap-5">
+						<li
+							onClick={() => {
+								setShowAbout(false);
+								setShowProjects(false);
+								setShowSkills(false);
+							}}
+							className="font-semibold cursor-pointer hover:text-white">
+							Home
+						</li>
+						<li
+							onClick={() => {
+								setShowAbout(true);
+								setShowProjects(false);
+								setShowSkills(false);
+							}}
+							className="font-semibold cursor-pointer hover:text-white">
+							About
+						</li>
+						<li
+							onClick={() => {
+								setShowAbout(false);
+								setShowProjects(false);
+								setShowSkills(true);
+							}}
+							className="font-semibold cursor-pointer hover:text-white">
+							Skill
+						</li>
+						<li className="font-semibold cursor-pointer hover:text-white">Education</li>
+						<li className="font-semibold cursor-pointer hover:text-white">Contact</li>
+					</ul>
+				</div>
+			</div>
+		</header>
+	);
 }
